b17c6d39fef2dfeed31675c4391bb11afd2034ff
Astenolite <vladcindea06@gmail.com>
2026-08-21 15:08:40 +0300

    Minor changes for standardization's sake

--- games/minecraft/commands/required/server_delete.py
+++ games/minecraft/commands/required/server_delete.py
@@ -27,14 +27,10 @@ class ServerDelete(Commands):
         interaction: discord.Interaction,
         server_name: str,
     ): 
-        context = {
-            "server_name": server_name,
-        }
-
         await interaction.response.defer(thinking=True)
 
         try:
-            await self.server_manager.delete_server(context)
+            await self.server_manager.delete_server(server_name)
 
             await interaction.followup.send(f"Server `{server_name}` deleted successfully.")
         except Exception as e:
