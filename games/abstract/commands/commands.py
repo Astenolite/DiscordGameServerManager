d626959190f8536d8e61d770eec55839b0efb292
Astenolite <vladcindea06@gmail.com>
2026-08-21 16:32:27 +0300

    Small changes in preparation for standard discord commands creation

--- games/abstract/commands/commands.py
+++ games/abstract/commands/commands.py
@@ -7,10 +7,10 @@ from games.abstract.game_server_manager import GameServerManager
 
 
 class Commands(ABC):
-
-    @abstractmethod
     def __init__(self, server_manager: GameServerManager):
         self.server_manager = server_manager
+        self.group_name = server_manager.config.group_name
+        self.game_name = server_manager.config.game_name
 
     # register commands
     @abstractmethod
