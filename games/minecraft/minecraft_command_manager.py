e28662df63750697fee0527c3a55bc0c22bb23a2
Astenolite <vladcindea06@gmail.com>
2026-08-21 16:35:55 +0300

    Updated command managers so that they make use of their parent class to reduce code duplication

--- games/minecraft/minecraft_command_manager.py
+++ games/minecraft/minecraft_command_manager.py
@@ -1,42 +1,37 @@
-from discord import app_commands
 from discord.app_commands import Group
 from games.abstract.game_command_manager import GameCommandManager
 
 from .minecraft_server_manager import MinecraftServerManager
 
-from .commands.required.server_create import ServerCreate
-from .commands.required.server_delete import ServerDelete
-from .commands.required.server_edit import ServerEdit
-from .commands.required.server_reset import ServerReset
-from .commands.required.server_backup import ServerBackup
-from .commands.required.server_list_backups import ServerListBackups
-from .commands.required.server_restore import ServerRestore
-from .commands.required.server_delete_backup import ServerDeleteBackup
+
+from .commands.required.server_create import MinecraftServerCreate
+from .commands.required.server_edit import MinecraftServerEdit
+from games.abstract.commands.required.server_delete import ServerDelete
+from games.abstract.commands.required.server_reset import ServerReset
+from games.abstract.commands.required.server_backup import ServerBackup
+from games.abstract.commands.required.server_list_backups import ServerListBackups
+from games.abstract.commands.required.server_restore import ServerRestore
+from games.abstract.commands.required.server_delete_backup import ServerDeleteBackup
 
 from .commands.optional.minecraft_commands import MinecraftCommands
 
-from .config.minecraft_config import MinecraftConfig
 
 class MinecraftCommandManager(GameCommandManager):
-    config = MinecraftConfig
 
-    def __init__(
-            self, 
-            server_manager: MinecraftServerManager,
-        ):
-            super().__init__(server_manager)
+    server_create_class = MinecraftServerCreate
+    server_delete_class = ServerDelete
+    server_edit_class = MinecraftServerEdit
+    server_reset_class = ServerReset
+    server_backup_class = ServerBackup
+    server_restore_class = ServerRestore
+    server_delete_backup_class = ServerDeleteBackup
+    server_list_backups_class = ServerListBackups
 
-            self.server_create = ServerCreate(self.server_manager)
-            self.server_delete = ServerDelete(self.server_manager)
-            self.server_edit = ServerEdit(self.server_manager)
-            self.server_reset = ServerReset(self.server_manager)
-            self.server_backup = ServerBackup(self.server_manager)
-            self.server_restore = ServerRestore(self.server_manager)
-            self.server_list_backups = ServerListBackups(self.server_manager)
-            self.server_delete_backup = ServerDeleteBackup(self.server_manager)
+    
+    def __init__(self, server_manager: MinecraftServerManager):
+        super().__init__(server_manager)
 
-            self.minecraft_commands = MinecraftCommands(self.server_manager)
-
+        self.game_commands = MinecraftCommands(server_manager)
 
 
     def register_commands(self,
@@ -50,13 +45,17 @@ class MinecraftCommandManager(GameCommandManager):
         delete_backup_group: Group,
         reset_group: Group,
     ):
-        self.server_create.register(create_group)
-        self.server_delete.register(delete_group)
-        self.server_edit.register(edit_group)
-        self.server_reset.register(reset_group)
-        self.server_backup.register(backup_group)
-        self.server_restore.register(restore_group)
-        self.server_list_backups.register(list_backups_group)
-        self.server_delete_backup.register(delete_backup_group)
 
-        self.minecraft_commands.register(game_group)
+        super().register_commands(
+            game_group=game_group,
+            create_group=create_group,
+            delete_group=delete_group,
+            edit_group=edit_group,
+            backup_group=backup_group,
+            restore_group=restore_group,
+            list_backups_group=list_backups_group,
+            delete_backup_group=delete_backup_group,
+            reset_group=reset_group
+        )
+
+        self.game_commands.register(game_group)
--- games/the_forest/the_forest_command_manager.py
+++ games/the_forest/the_forest_command_manager.py
@@ -1,58 +1,25 @@
-from discord import app_commands
-from discord.app_commands import Group
 from games.abstract.game_command_manager import GameCommandManager
 
-from .the_forest_server_manager import TheForestServerManager
 
-from .commands.required.server_create import ServerCreate
-from .commands.required.server_delete import ServerDelete
-from .commands.required.server_edit import ServerEdit
-from .commands.required.server_reset import ServerReset
-from .commands.required.server_backup import ServerBackup
-from .commands.required.server_list_backups import ServerListBackups
-from .commands.required.server_restore import ServerRestore
-from .commands.required.server_delete_backup import ServerDeleteBackup
+from .commands.required.server_create import TheForestServerCreate
+from .commands.required.server_edit import TheForestServerEdit
+from games.abstract.commands.required.server_delete import ServerDelete
+from games.abstract.commands.required.server_reset import ServerReset
+from games.abstract.commands.required.server_backup import ServerBackup
+from games.abstract.commands.required.server_list_backups import ServerListBackups
+from games.abstract.commands.required.server_restore import ServerRestore
+from games.abstract.commands.required.server_delete_backup import ServerDeleteBackup
 
 
-from .config.the_forest_config import TheForestConfig
 
 class TheForestCommandManager(GameCommandManager):
-    config = TheForestConfig
 
-    def __init__(
-            self, 
-            server_manager: TheForestServerManager,
-        ):
-            super().__init__(server_manager)
-
-            self.server_create = ServerCreate(self.server_manager)
-            self.server_delete = ServerDelete(self.server_manager)
-            self.server_edit = ServerEdit(self.server_manager)
-            self.server_reset = ServerReset(self.server_manager)
-            self.server_backup = ServerBackup(self.server_manager)
-            self.server_restore = ServerRestore(self.server_manager)
-            self.server_list_backups = ServerListBackups(self.server_manager)
-            self.server_delete_backup = ServerDeleteBackup(self.server_manager)
-
-
-
-
-    def register_commands(self,
-        game_group: Group,
-        create_group: Group,
-        delete_group: Group,
-        edit_group: Group,
-        backup_group: Group,
-        restore_group: Group,
-        list_backups_group: Group,
-        delete_backup_group: Group,
-        reset_group: Group,
-    ):
-        self.server_create.register(create_group)
-        self.server_delete.register(delete_group)
-        self.server_edit.register(edit_group)
-        self.server_reset.register(reset_group)
-        self.server_backup.register(backup_group)
-        self.server_restore.register(restore_group)
-        self.server_list_backups.register(list_backups_group)
-        self.server_delete_backup.register(delete_backup_group)
+    server_create_class = TheForestServerCreate
+    server_delete_class = ServerDelete
+    server_edit_class = TheForestServerEdit
+    server_reset_class = ServerReset
+    server_backup_class = ServerBackup
+    server_restore_class = ServerRestore
+    server_delete_backup_class = ServerDeleteBackup
+    server_list_backups_class = ServerListBackups
+        
