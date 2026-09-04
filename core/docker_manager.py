b5e7aa9d1f280b0a8868d3135ca10541b5836d10
Astenolite <vladcindea06@gmail.com>
2026-09-04 11:52:00 +0300

    Changed some functions to use the docker API

--- core/docker_manager.py
+++ core/docker_manager.py
@@ -1,7 +1,11 @@
 import subprocess
 import json
+import docker
+import asyncio
+
 from datetime import datetime, timezone
 from pathlib import Path
+from docker.models.containers import Container
 
 def parse_docker_timestamp(timestamp: str) -> datetime:
     if "." in timestamp:
@@ -12,6 +16,28 @@ def parse_docker_timestamp(timestamp: str) -> datetime:
     return datetime.fromisoformat(timestamp.replace("Z", "+00:00"))
 
 class DockerManager:
+
+    def __init__(self):
+        self.client = docker.from_env()
+
+
+    async def get_container(self, container_name: str) -> Container:
+        container = await asyncio.to_thread(
+            self.client.containers.get,
+            container_name,
+        )
+        return container
+
+    async def container_exists(self, container_name: str) -> bool:
+        if await self.get_container(container_name) is None:
+            return False
+        return True
+    
+    async def container_isOnline(self, container_name: str) -> bool:
+        if not await self.container_exists(container_name):
+            raise ValueError(f"Container {container_name} does not exist")
+
+        container = self.get_container(container_name)
     
     async def create(self, compose_file: Path) -> None:
         subprocess.run(
@@ -49,8 +75,7 @@ class DockerManager:
             check=True
         )
     
-
-    async def compose_down(self, compose_file: Path):
+    async def compose_down(self, compose_file: Path) -> None:
         subprocess.run(
             [
                 "docker",
@@ -63,20 +88,7 @@ class DockerManager:
             check=True,
         )
 
-    async def container_exists(self, container_name: str) -> bool:
-        result = subprocess.run(
-            [
-                "docker", 
-                "container", 
-                "inspect", 
-                container_name
-            ],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-
-        return result.returncode == 0
-
+    """
     async def is_online(self, container_name: str) -> bool:
         if not await self.container_exists(container_name):
             return False
@@ -95,46 +107,23 @@ class DockerManager:
         return result.stdout.strip() == "true"
     
     async def start(self, container_name: str):
-        subprocess.run(
-            [
-                "docker",
-                "start",
-                container_name
-            ],
-            check=True
-        )
+        container = await self.get_container(container_name)
+        container.start()
+    """
 
     async def stop(self, container_name: str):
-        subprocess.run(
-            [
-                "docker",
-                "stop",
-                container_name
-            ],
-            check=True
-        )
+        container = await self.get_container(container_name)
+        container.stop()
 
-    async def restart(self, container_name):
-        subprocess.run(
-            [
-                "docker",
-                "restart",
-                container_name
-            ],
-            check=True
-        )
+    async def restart(self, container_name: str):
+        container = await self.get_container(container_name)
+        container.restart()
 
     async def status(self, container_name) -> dict:
-        result = subprocess.run(
-            [
-                "docker",
-                "inspect",
-                container_name,
-            ],
-            check=True,
-            capture_output=True,
-            text=True,
-        )
+        container = await self.get_container(container_name)
+
+        result = container.status
+        print(container.status, flush=True)
 
         container = json.loads(result.stdout)[0]
         state = container["State"]
@@ -164,20 +153,13 @@ class DockerManager:
             "health": state.get("Health", {}).get("Status"),
         }
     
-    async def execute_command(self, container_name: str, command: list[str]):
-        result = subprocess.run(
-            [
-                "docker",
-                "exec",
-                container_name,
-                *command
-            ],
-            check=True,
-            capture_output=True,
-            text=True,
-        )
+    async def execute_command(self, container_name: str, command: list[str]) -> int:
+        container = await self.get_container(container_name)
+        result = container.exec_run(command)
+        return result.exit_code
 
-        return result.stdout.strip()
-        
-
-        
+    async def get_container_image(self, container_name: str) -> str:
+        container = await self.get_container(container_name)
+        image = container.attrs["Config"]["Image"]
+        image_name = image.rsplit(":", 1)[0]
+        return image_name
